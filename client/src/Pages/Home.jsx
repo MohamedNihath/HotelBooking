23ee9d0bbe97b65e1fd27ffbd04ed757c01da05f
MohamedNihath <mohamednihath0308@gmail.com>
2025-06-18 18:05:13 +0530

    CompleteFeatureDestinationANDtitleComponents

--- client/src/Pages/Home.jsx
+++ client/src/Pages/Home.jsx
@@ -1,10 +1,12 @@
 import React from 'react'
 import Hero from '../Components/Hero'
+import FetureDestination from '../Components/FetureDestination'
 
 const Home = () => {
   return (
     <>
       <Hero/>
+      <FetureDestination/>
     </>
   )
 }
