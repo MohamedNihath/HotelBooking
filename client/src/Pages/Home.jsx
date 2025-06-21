112212f3f81429ab09364004a21d356f4d51e3e5
MohamedNihath <mohamednihath0308@gmail.com>
2025-06-21 09:43:50 +0530

    CompletetTheExclusiveOfferSection

--- client/src/Pages/Home.jsx
+++ client/src/Pages/Home.jsx
@@ -1,12 +1,14 @@
 import React from 'react'
 import Hero from '../Components/Hero'
 import FetureDestination from '../Components/FetureDestination'
+import ExclusiveOffer from '../Components/ExclusiveOffer'
 
 const Home = () => {
   return (
     <>
       <Hero/>
       <FetureDestination/>
+      <ExclusiveOffer/>
     </>
   )
 }
