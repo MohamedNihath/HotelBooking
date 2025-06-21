f64bcaa5ad4e67d6865676a15420ea61013edf06
MohamedNihath <mohamednihath0308@gmail.com>
2025-06-21 10:18:34 +0530

    CompleteTheTestimonialSection

--- client/src/Pages/Home.jsx
+++ client/src/Pages/Home.jsx
@@ -2,6 +2,7 @@ import React from 'react'
 import Hero from '../Components/Hero'
 import FetureDestination from '../Components/FetureDestination'
 import ExclusiveOffer from '../Components/ExclusiveOffer'
+import Testimonial from '../Components/Testimonial'
 
 const Home = () => {
   return (
@@ -9,6 +10,7 @@ const Home = () => {
       <Hero/>
       <FetureDestination/>
       <ExclusiveOffer/>
+      <Testimonial/>
     </>
   )
 }
