32f2e02904c0eaf29e21c89342038c46d4edf695
MohamedNihath <mohamednihath0308@gmail.com>
2025-07-07 10:15:54 +0530

    Complete the Admins DashBoard Section

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -7,6 +7,10 @@ import AllRooms from './Pages/AllRooms'
 import RoomDetails from './Pages/RoomDetails'
 import MyBookings from './Pages/MyBookings'
 import HotelReg from './Components/HotelReg'
+import Layout from './Pages/hotelOwner/Layout'
+import DashBoard from './Pages/hotelOwner/DashBoard'
+import AddRoom from './Pages/hotelOwner/AddRoom'
+import ListRoom from './Pages/hotelOwner/ListRoom'
 
 const App = () => {
 
@@ -25,6 +29,11 @@ const isOwnerPath = useLocation().pathname.includes("owner")
               <Route path='/rooms' element={<AllRooms/>}/>
               <Route path='/rooms/:id' element={<RoomDetails/>}/>
               <Route path='/my-booking' element={<MyBookings/>}/>
+              <Route path='/owner' element={<Layout/>}>
+                <Route index element={<DashBoard/>}/>
+                <Route path='add-room' element={<AddRoom/>}/>
+                <Route path='list-room' element={<ListRoom/>}/>
+              </Route>
 
 
       </Routes>
